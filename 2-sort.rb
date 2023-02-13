a8b18f9857a8ddb7b66d46cce30c3e18b9261a9e
theotsori <theodoretsori@gmail.com>
2023-02-13 09:07:10 +0300

    ruby sort

--- 2-sort.rb
+++ 2-sort.rb
@@ -11,21 +11,22 @@ ARGV.each do |arg|
 
     # convert to integer
     i_arg = arg.to_i
+    if i_arg.to_s == arg
     
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
+        # insert result at the right position
+        is_inserted = false
+        i = 0
+        l = result.size
+        while i < l && !is_inserted do
+            if result[i] >= i_arg
+                result.insert(i, i_arg)
+                is_inserted = true
+            else
+                i += 1
+            end
         end
+        result << i_arg if !is_inserted
     end
-    result << i_arg if !is_inserted
 end
 
 puts result
